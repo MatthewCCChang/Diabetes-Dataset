c10ce7c38212eed252bb856bd964c14590f6d969
Matthew <deadwalker1228@gmail.com>
2024-05-21 20:50:36 -0700

    Created functions for sanitizing data

---
+++ sanitize.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+def read_data():
+    data = pd.read_csv('diabetic_data.csv')
+    return data
+
+def extract_data(data):
+    basic_data = data[['race','age','gender','diag_1']]
+    #add other stuff here for other types of data needed to extract
+    return basic_data
+
+if __name__ == '__main__':
+    data = read_data()
+    basic = extract_data(data)
