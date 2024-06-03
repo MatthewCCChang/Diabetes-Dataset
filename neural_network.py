1431448654961a2617d19e9c9bc853e822049880
Matthew <deadwalker1228@gmail.com>
2024-06-03 15:42:09 -0700

    Cleaned up data for analyzing

--- neural_network.py
+++ neural_network.py
@@ -1,16 +1,37 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
 
-def startData(data):
-    X = data.iloc[:,:3]  #get first three cols of data
-    y = data.iloc[:,:4]  #get last col = diagnosis
-    X_tr, y_tr, X_va, y_va, X_te, y_te = separateData(X,y)  #is Dataframe
-    # print(type(X_tr))
+diabetes_code = [2535,3572,5881,64800,64801,64802,64803,64804,7751,"V1221"]
+
+def startData(data):  #input is df
+    # data = data.to_numpy()
+    # print(type(data))
+    # print(len(data))
+    # valid_idx = y.__index__
+    X = preprocess_data(data[:,:3])  #get first three cols of data
+    y = preprocess_label(data[:,3])  #get last col = diagnosis
+    # print(type(y))    #ndarray
+    # print(len(X))
+    # print(len(y))
+    X_tr, y_tr, X_va, y_va, X_te, y_te = separateData(X,y)  #
+    print(type(X_tr))
 
 def separateData(X_data, y_data):
     X_temp, X_te, y_temp, y_te = train_test_split(X_data, y_data, test_size=0.2 ,random_state=1234, shuffle=True)
     X_tr, X_va, y_tr, y_va = train_test_split(X_temp, y_temp, test_size=0.15 ,shuffle=True, random_state=1234)
     return X_tr, y_tr, X_va, y_va, X_te, y_te
     
-    
+def preprocess_data(data):
+    # data.replace('?',"other",inplace=True)
+    encoder = OneHotEncoder(sparse_output=False)
+    encoded_columns = [encoder.fit_transform(data[:, [i]]) for i in range(data.shape[1])]
+    encoded_data = np.hstack(encoded_columns)
+    return encoded_data
+
+def preprocess_label(labels):
+    # labels = labels[labels != '?']
+    lb = LabelBinarizer()
+    binary_label = lb.fit_transform(labels.reshape(-1,1))
+    return binary_label
--- sanitize.py
+++ sanitize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import neural_network as nn
+from sklearn.impute import SimpleImputer
 
 def read_data():
     data = pd.read_csv('diabetic_data.csv')
@@ -8,8 +9,22 @@ def read_data():
 
 def extract_data(data):
     basic_data = data[['race','age','gender','diag_1']]
+    basic_data = basic_data[basic_data['diag_1'] != '?']
+    
+    race = basic_data[['race']]
+    other = basic_data[['age','gender','diag_1']]
+    
+    imp = SimpleImputer(missing_values='?', strategy="constant", fill_value="other")
+    clean_race = imp.fit_transform(race)
+    # print(len(clean_race))
+    np_data = other.to_numpy()
+    # print(len(np_data))
+    clean_data = np.concatenate((clean_race, np_data),axis=1)
+    # print(len(clean_data))
     #add other stuff here for other types of data needed to extract
-    return basic_data
+    return clean_data
+
+
 
 if __name__ == '__main__':
     data = read_data()
